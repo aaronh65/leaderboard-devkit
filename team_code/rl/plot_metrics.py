7b267ea78b258a3e67ec739e2bbf6abddc76516e
aaron <aaronh65@gmail.com>
2021-01-25 23:32:09 -0800

    metrics plotting works with new log structure

--- team_code/rl/plot_metrics.py
+++ team_code/rl/plot_metrics.py
@@ -1,28 +1,27 @@
 import argparse
+import json
 import matplotlib.pyplot as plt
 import numpy as np
 
 def main(args):
 
-    metrics = []
-    metric_names = ['rewards', 'policy_losses', 'value_losses', 'entropies']
-    for name in metric_names:
-        with open(f'{args.target_dir}/{name}.npy', 'rb') as f:
-            metrics.append(np.load(f))
+    with open(f'{args.target_dir}/logs/log.json', 'r') as f:
+        log = json.load(f)['checkpoints']
+    rewards = []
+    for episode_ckpt in log:
+        rewards.append(episode_ckpt['sac']['total_reward'])
 
-    rewards, _, _, _ = metrics
     episodes = np.arange(len(rewards))
     plt.plot(episodes, rewards)
     plt.xlabel('episode #')
-    plt.ylabel('episode reward')
+    plt.ylabel('episode total reward')
     plt.title ('training episode rewards')
     #plt.show()
-    plt.savefig(f'{args.target_dir}/result.png')
+    plt.savefig(f'{args.target_dir}/total_rewards.png')
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--target_dir', type=str, required=True)
-    parser.add_argument('--load_from', type=str)
     args = parser.parse_args()
     return args
 
