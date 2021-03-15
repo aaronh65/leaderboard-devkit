ecb3ce709d35d35059e25e5a378148754cf959c7
Aaron Huang <aaronhuang@cmu.edu>
2021-03-15 15:23:56 -0400

    changed make video script to work with new debugging structure. move to team code scripts?

--- team_code/lbc/scripts/make_videos.py
+++ team_code/lbc/scripts/make_videos.py
@@ -1,44 +1,37 @@
-import os
 import cv2
-import argparse
+import os, argparse
+from pathlib import Path
 
 def main(args):
 
-    video_dir = os.path.join(args.target_dir, 'videos')
-    image_dir = os.path.join(args.target_dir, 'images')
-    assert os.path.exists(image_dir), 'no image directory'
-    routes = sorted(os.listdir(image_dir))
+    videos = Path(f'{args.target_dir}/videos')
+    routes = Path(f'{args.target_dir}/data').glob('*')
 
     width, height = None, None
-    for route in routes:
-
-        route_image_dir = os.path.join(image_dir, route)
-        input_dirs = [os.path.join(route_image_dir, dir_name) for dir_name in sorted(os.listdir(route_image_dir))]
-        input_dirs = [dir_name for dir_name in input_dirs if 'bkup' not in dir_name and 'mp4' not in dir_name]
-        for input_dir in input_dirs:
+    for route in sorted(list(routes)):
+        route_reps = sorted(list(route.glob('*')))
+        for dir_name in route_reps:
             
             # get save_path
-            tokens = input_dir.split('/')
-            route, repetition = tokens[-2:]
-            route_video_dir = os.path.join(video_dir, route)
-            if not os.path.exists(route_video_dir):
-                os.makedirs(route_video_dir)
-            save_path = os.path.join(route_video_dir, f'{repetition}.mp4')
+            route, repetition = dir_name.parts[-2:]
+            video_dir = videos / route
+            video_dir.mkdir(parents=True, exist_ok=True)
+
+            save_path = str(video_dir / f'{repetition}.mp4')
+            debug_path = dir_name / 'debug'
 
             if width is None or height is None:
-                fname = os.listdir(input_dir)[0]
-                impath = os.path.join(input_dir, fname)
-                im = cv2.imread(impath)
+                fname = sorted(list(debug_path.glob('*')))[0]
+                im = cv2.imread(str(fname))
                 height, width, channels = im.shape
 
             if width % 2 == 1 or height % 2 == 1:
-                cmd = f'ffmpeg -y -r 2 -s {width}x{height} -f image2 -i {input_dir}/%06d.png -pix_fmt yuv420p -vf \"pad=ceil(iw/2)*2:ceil(ih/2)*2\" {save_path}'
+                cmd = f'ffmpeg -y -r 2 -s {width}x{height} -f image2 -i {debug_path}/%06d.png -pix_fmt yuv420p -vf \"pad=ceil(iw/2)*2:ceil(ih/2)*2\" {save_path}'
             else:
-                cmd = f'ffmpeg -y -r 2 -s {width}x{height} -f image2 -i {input_dir}/%06d.png -pix_fmt yuv420p {save_path}'
-
+                cmd = f'ffmpeg -y -r 2 -s {width}x{height} -f image2 -i {debug_path}/%06d.png -pix_fmt yuv420p {save_path}'
 
             # make video
-            cmd = f'ffmpeg -y -r 2 -s 1627x256 -f image2 -i {input_dir}/%06d.png -pix_fmt yuv420p -vf \"pad=ceil(iw/2)*2:ceil(ih/2)*2\" {save_path}'
+            #cmd = f'ffmpeg -y -r 2 -s 1627x256 -f image2 -i {input_dir}/%06d.png -pix_fmt yuv420p -vf \"pad=ceil(iw/2)*2:ceil(ih/2)*2\" {save_path}'
             os.system(cmd)
 
 def parse_args():
